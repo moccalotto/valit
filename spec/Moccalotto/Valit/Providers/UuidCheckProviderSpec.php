2a91fef248dca14b2b1b43472006c76fa1856fa7
Kim Ravn Hansen <moccalotto@gmail.com>
2016-09-27 07:10:17 +0200

    Add Uuid verison checker.
    Improve test coverage.

--- spec/Moccalotto/Valit/Providers/UuidCheckProviderSpec.php
+++ spec/Moccalotto/Valit/Providers/UuidCheckProviderSpec.php
@@ -20,14 +20,58 @@ class UuidCheckProviderSpec extends ObjectBehavior
 
         $this->checkIsUuid('')->shouldHaveType('Moccalotto\Valit\Result');
 
-        $this->checkIsUuid('00000000-0000-0000-0000-000000000000')->success()->shouldBe(true);
+        $this->checkIsUuid('00000000-0000-1000-8000-000000000000')->success()->shouldBe(true);
+        $this->checkIsUuid('00000000-0000-2000-9000-000000000000')->success()->shouldBe(true);
+        $this->checkIsUuid('00000000-0000-3000-a000-000000000000')->success()->shouldBe(true);
+        $this->checkIsUuid('00000000-0000-4000-b000-000000000000')->success()->shouldBe(true);
+        $this->checkIsUuid('00000000-0000-5000-a000-000000000000')->success()->shouldBe(true);
         $this->checkIsUuid('581a8d5f-68b0-400f-a96f-b2c7534cff52')->success()->shouldBe(true);
 
+        $this->checkisuuid('c56a4180-65aa-42ec-a945-5fd21dec')->success()->shouldbe(false);
         $this->checkIsUuid('11111111-1111-1111-1111-111111111111')->success()->shouldBe(false);
         $this->checkIsUuid('x56a4180-h5aa-42ec-a945-5fd21dec0538')->success()->shouldBe(false);
-        $this->checkIsUuid('C56A4180-65AA-42EC-A945-5FD21DEC')->success()->shouldBe(false);
-        $this->checkIsUuid(null)->success()->shouldBe(false);
+        $this->checkIsUuid('00000000-0000-1000-c000-000000000000')->success()->shouldBe(false);
+        $this->checkIsUuid('00000000-0000-2000-d000-000000000000')->success()->shouldBe(false);
+        $this->checkIsUuid('00000000-0000-3000-e000-000000000000')->success()->shouldBe(false);
+        $this->checkIsUuid('00000000-0000-4000-f000-000000000000')->success()->shouldBe(false);
+        $this->checkIsUuid('00000000-0000-5000-1000-000000000000')->success()->shouldBe(false);
 
+        $this->checkIsUuid(1)->success()->shouldBe(false);
         $this->checkIsUuid('')->success()->shouldBe(false);
+        $this->checkIsUuid([])->success()->shouldBe(false);
+        $this->checkIsUuid(1.0)->success()->shouldBe(false);
+        $this->checkIsUuid(null)->success()->shouldBe(false);
+        $this->checkIsUuid('foo')->success()->shouldBe(false);
+        $this->checkIsUuid((object) [])->success()->shouldBe(false);
+        $this->checkIsUuid(curl_init())->success()->shouldBe(false);
+    }
+
+    function it_checks_uuidVersion()
+    {
+        $this->provides()->shouldHaveKey('isUuidVersion');
+        $this->provides()->shouldHaveKey('uuidVersion');
+
+        $this->checkUuidVersion('', 1)->shouldHaveType('Moccalotto\Valit\Result');
+
+        $this->checkUuidVersion('00000000-0000-1000-8000-000000000000', 1)->success()->shouldBe(true);
+        $this->checkUuidVersion('00000000-0000-2000-8000-000000000000', 2)->success()->shouldBe(true);
+        $this->checkUuidVersion('00000000-0000-3000-8000-000000000000', 3)->success()->shouldBe(true);
+        $this->checkUuidVersion('00000000-0000-4000-8000-000000000000', 4)->success()->shouldBe(true);
+        $this->checkUuidVersion('00000000-0000-5000-8000-000000000000', 5)->success()->shouldBe(true);
+
+        $this->checkUuidVersion('00000000-0000-0000-0000-000000000000', 1)->success()->shouldBe(false);
+        $this->checkUuidVersion('00000000-0000-0000-0000-000000000000', 2)->success()->shouldBe(false);
+        $this->checkUuidVersion('00000000-0000-0000-0000-000000000000', 3)->success()->shouldBe(false);
+        $this->checkUuidVersion('00000000-0000-0000-0000-000000000000', 4)->success()->shouldBe(false);
+        $this->checkUuidVersion('00000000-0000-0000-0000-000000000000', 5)->success()->shouldBe(false);
+
+        $this->checkIsUuid(1)->success()->shouldBe(false);
+        $this->checkIsUuid('')->success()->shouldBe(false);
+        $this->checkIsUuid([])->success()->shouldBe(false);
+        $this->checkIsUuid(1.0)->success()->shouldBe(false);
+        $this->checkIsUuid(null)->success()->shouldBe(false);
+        $this->checkIsUuid('foo')->success()->shouldBe(false);
+        $this->checkIsUuid((object) [])->success()->shouldBe(false);
+        $this->checkIsUuid(curl_init())->success()->shouldBe(false);
     }
 }
--- src/Providers/UuidCheckProvider.php
+++ src/Providers/UuidCheckProvider.php
@@ -21,6 +21,33 @@ class UuidCheckProvider implements CheckProvider
     use ProvideViaReflection;
 
     /**
+     * @param mixed $value
+     *
+     * @return Result
+     */
+    protected function parseUuid($value, &$version = null, &$variant = null)
+    {
+        if (!is_string($value)) {
+            return false;
+        }
+
+        if ($value === '00000000-0000-0000-0000-000000000000') {
+            return true;
+        }
+
+        $isMatch = preg_match(
+            '/[0-9a-f]{8}-[0-9a-f]{4}-([1-5])[0-9a-f]{3}-([89ab])[0-9a-f]{3}-[0-9a-f]{12}$/Ai',
+            $value,
+            $matches
+        );
+
+        $version = $isMatch ? $matches[1] : null;
+        $variant = $isMatch ? $matches[2] : null;
+
+        return $isMatch;
+    }
+
+    /**
      * Check if $value is a uuid.
      *
      * @Check(["isUuid", "uuid"])
@@ -31,11 +58,28 @@ class UuidCheckProvider implements CheckProvider
      */
     public function checkIsUuid($value)
     {
-        $success = is_string($value) && (
-            $value === '00000000-0000-0000-0000-000000000000'
-            || preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i', $value)
-        );
+        return new Result($this->parseUuid($value), '{name} must be a valid UUID');
+    }
 
-        return new Result($success, '{name} must be a valid UUID');
+    /**
+     * Check if $value is a uuid.
+     *
+     * @Check(["isUuidVersion", "uuidVersion"])
+     *
+     * @param mixed $value
+     *
+     * @return Result
+     */
+    public function checkUuidVersion($value, $version)
+    {
+        $version = (int) $version;
+
+        $match = $this->parseUuid($value, $parsedVersion);
+
+        if ($version < 1 || $version > 5) {
+            throw new InvalidArgumentException('$version must be an integer in the range [1..5]');
+        }
+
+        return new Result($match && $parsedVersion == $version, '{name} must be a version {0} UUID', [$version]);
     }
 }
