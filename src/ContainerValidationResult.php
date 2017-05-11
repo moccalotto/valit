1d867c970707f545b95dc4eabf6525c5c0a14452
Kim Ravn Hansen <moccalotto@gmail.com>
2017-05-11 06:30:35 +0200

    Docs

--- src/ContainerValidationResult.php
+++ src/ContainerValidationResult.php
@@ -1,7 +1,19 @@
 <?php
 
+/**
+ * This file is part of the Valit package.
+ *
+ * @package Valit
+ * @author Kim Ravn Hansen <moccalotto@gmail.com>
+ * @copyright 2016
+ * @license MIT
+ */
+
 namespace Moccalotto\Valit;
 
+/**
+ * Result of validating a container.
+ */
 class ContainerValidationResult
 {
     /**
