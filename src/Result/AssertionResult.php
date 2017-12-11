d6f82b1b4e0bc797da911872d1e4d10614bebfed
Kim Ravn Hansen <moccalotto@gmail.com>
2017-12-11 16:47:47 +0100

    Refactor

--- src/Result/AssertionResult.php
+++ src/Result/AssertionResult.php
@@ -2,8 +2,7 @@
 
 namespace Valit\Result;
 
-use LogicException;
-use RuntimeException;
+use Valit\Util\VarDumper;
 
 /**
  * Valit Result.
@@ -76,45 +75,6 @@ class AssertionResult
     }
 
     /**
-     * Format a value for being displayed as a string in an error message.
-     *
-     * @param mixed $value
-     *
-     * @return string
-     */
-    protected function escape($value)
-    {
-        if (is_scalar($value)) {
-            return json_encode($value, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-        }
-
-        if (is_callable($value)) {
-            return 'Callable';
-        }
-
-        if (is_resource($value)) {
-            return sprintf('%s (%s)', $value, get_resource_type($value));
-        }
-
-        if (is_object($value)) {
-            return sprintf('Object (%s)', get_class($value));
-        }
-
-        if (is_array($value)) {
-            return sprintf('Array (%d entries)', count($value));
-        }
-
-        if (is_null($value)) {
-            return 'null';
-        }
-
-        throw new RuntimeException(sprintf(
-            'Unknown type: %s',
-            gettype($value)
-        ));
-    }
-
-    /**
      * Render the error mesage, using the name and alias provided.
      *
      * @param string $name
@@ -142,49 +102,9 @@ class AssertionResult
                     return $all;
                 }
 
-                return $this->format($context[$key], $fmt);
+                return VarDumper::format($context[$key], $fmt);
             },
             $this->message
         );
-    }
-
-    /**
-     * Format a given value into a string.
-     *
-     * @param mixed  $value
-     * @param string $format
-     *
-     * @return string
-     *
-     * @throws LogicException if $format is not known
-     */
-    protected function format($value, $format)
-    {
-        if ($format === 'normal') {
-            return $this->escape($value);
-        }
-
-        if ($format === 'raw') {
-            return is_scalar($value) || is_callable([$value, '__toString'])
-                ? (string) $value
-                : $this->escape($value);
-        }
-
-        if ($format === 'type') {
-            return gettype($value);
-        }
-
-        if ($format === 'float') {
-            return is_numeric($value)
-                ? sprintf('%g', $value)
-                : '[not numeric]';
-        }
-        if ($format === 'hex') {
-            return is_int($value) || ctype_digit($value)
-                ? sprintf('%x', $value)
-                : '[not integer]';
-        }
-
-        throw new LogicException("Unknown format »{$format}«");
     }
 }
---
+++ src/Util/VarDumper.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Valit\Util;
+
+use LogicException;
+use RuntimeException;
+
+class VarDumper
+{
+    /**
+     * Format a given value into a string.
+     *
+     * @param mixed  $value
+     * @param string $format
+     *
+     * @return string
+     *
+     * @throws LogicException if $format is not known
+     */
+    public static function format($value, $format)
+    {
+        if ($format === 'normal') {
+            return static::escape($value);
+        }
+
+        if ($format === 'raw') {
+            return is_scalar($value) || is_callable([$value, '__toString'])
+                ? (string) $value
+                : static::escape($value);
+        }
+
+        if ($format === 'type') {
+            return gettype($value);
+        }
+
+        if ($format === 'float') {
+            return is_numeric($value)
+                ? sprintf('%g', $value)
+                : '[not numeric]';
+        }
+        if ($format === 'hex') {
+            return is_int($value) || ctype_digit($value)
+                ? sprintf('%x', $value)
+                : '[not integer]';
+        }
+
+        throw new LogicException("Unknown format »{$format}«");
+    }
+
+    /**
+     * Format a value for being displayed as a string in an error message.
+     *
+     * @param mixed $value
+     *
+     * @return string
+     */
+    public static function escape($value)
+    {
+        if (is_scalar($value)) {
+            return json_encode($value, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        }
+
+        if (is_callable($value)) {
+            return 'Callable';
+        }
+
+        if (is_resource($value)) {
+            return sprintf('%s (%s)', $value, get_resource_type($value));
+        }
+
+        if (is_object($value)) {
+            return sprintf('Object (%s)', get_class($value));
+        }
+
+        if (is_array($value)) {
+            return sprintf('Array (%d entries)', count($value));
+        }
+
+        if (is_null($value)) {
+            return 'null';
+        }
+
+        throw new RuntimeException(sprintf(
+            'Unknown type: %s',
+            gettype($value)
+        ));
+    }
+}
