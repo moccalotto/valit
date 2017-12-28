bc73ca2c774fca18cd65edc4d439cecd2b841766
Kim Ravn Hansen <moccalotto@gmail.com>
2017-12-28 15:24:39 +0100

    Semi realistic example

---
+++ examples/container-fluent-intro.php
@@ -0,0 +1,25 @@
+<?php
+
+use Valit\Value;
+use Valit\Ensure;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$assertions =  [
+    'username'      => Value::isString()->shorterThan(256)->longerThan(2),
+    'password'      => Value::isString()->shorterThan(65536)->isLongerThan(4),
+    'remember_me'   => Value::isOptional()->isOneOf(['yes', 'no']),
+    'csrf_token'    => Value::isHexString()->hasLength(40),
+];
+
+$container = [
+    'username' => 'foobar',
+    'password' => 'secr37',
+    'remember_me' => 'yes',
+    'csrf_token' => '4f0a8c629e23d947bb369cf420607947c24dc9a9',
+];
+
+
+$checks = Ensure::that($container)->contains($assertions);
+
+print 'All fields are valid';
