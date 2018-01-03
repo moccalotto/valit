b2087566c0b8e997ba2abf7cb799a67568532479
Kim Ravn Hansen <moccalotto@gmail.com>
2018-01-03 10:11:30 +0100

    Examples

--- examples/05-fluent-containers.php
+++ examples/05-fluent-containers.php
@@ -1,25 +1,41 @@
 <?php
 
 use Valit\Value;
-use Valit\Ensure;
+use Valit\Check;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$varCharLength = 255;
+$textFieldLength = 65535;
+$sha1Length = 40;
+$rememberOptions = ['yes', 'no', '1', '0', ''];
+
 $assertions =  [
-    'username'      => Value::isString()->shorterThan(256)->longerThan(2),
-    'password'      => Value::isString()->shorterThan(65536)->isLongerThan(4),
-    'remember_me'   => Value::isOptional()->isOneOf(['yes', 'no']),
-    'csrf_token'    => Value::isHexString()->hasLength(40),
+    'username'      => Value::lengthIs('<=', $varCharLength)->longerThan(2),
+    'password'      => Value::lengthIs('<=', $textFieldLength)->longerThan(4),
+    'remember_me'   => Value::isOptional()->isOneOf($rememberOptions),
+    'csrf_token'    => Value::isHexString()->hasLength($sha1Length),
 ];
 
-$container = [
+$postData = [
     'username' => 'foobar',
     'password' => 'secr37',
-    'remember_me' => 'yes',
-    'csrf_token' => '4f0a8c629e23d947bb369cf420607947c24dc9a9',
+    'remember_me' => 'foo',
+    'csrf_token' => 'this is not hex',
 ];
 
 
-$checks = Ensure::that($container)->contains($assertions);
+$checks = Check::that($postData)->contains($assertions);
 
-print 'No exceptions thrown, all fields are valid';
+print_r(
+    $checks->errorMessages()
+);
+
+/*
+    Array
+    (
+        [0] => remember_me must be one of "yes", "no", "1", "0", ""
+        [1] => csrf_token must contain only hexidecimal characters
+        [2] => csrf_token must be a string that has the length 40
+    )
+ */
