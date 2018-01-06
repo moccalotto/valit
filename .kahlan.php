32069e5d1129f16a4004b316a4f66f6036518316
Kim Ravn Hansen <krh@cego.dk>
2018-01-06 17:29:52 +0100

    coverage

--- .kahlan.php
+++ .kahlan.php
@@ -13,3 +13,28 @@ if (!extension_loaded('xdebug')) {
 // Override certain defaults if xdebug is enabled.
 $commandLine->option('coverage', 'default', 3);
 $commandLine->option('clover', 'default', 'coverage.xml');
+$commandLine->option('coverage-coveralls', 'default', 'coveralls.json');
+
+
+// Apply the logic to the `'reporting'` entry point.
+Filters::apply($this, 'reporting', function($chain) {
+
+    // Get the reporter called `'coverage'` from the list of reporters
+    $reporter = $this->reporters()->get('coverage');
+
+    // Abort if no coverage is available.
+    if (!$reporter || !$this->commandLine()->exists('coverage-coveralls')) {
+        return $chain->next();
+    }
+
+    // Use the `Coveralls` class to write the JSON coverage into a file
+    Coveralls::write([
+        'collector' => $reporter,
+        'file' => $this->commandLine()->get('coverage-coveralls'),
+        'service_name' => 'travis-ci',
+        'service_job_id' => getenv('TRAVIS_JOB_ID') ?: null
+    ]);
+
+    // Continue the chain
+    return $chain->next();
+});
