f33e72d2198ffaa10c9b123675fa1fe7507d6bc3
Kim Ravn Hansen <moccalotto@gmail.com>
2018-01-08 13:55:17 +0100

    more kahlan coverage

---
+++ tests/unit/Validators/ValueValidator.spec.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Kahlan\Spec\Suite;
+
+use Valit\Check;
+use Valit\Logic;
+use Valit\Manager;
+use Valit\Validators\ValueValidator;
+
+describe('ValueValidator', function () {
+    $subject = new ValueValidator(
+        Manager::instance(),
+        null,
+        false
+    );
+
+    describe('varName', function () use ($subject) {
+        it('has a sane default value', function () use ($subject) {
+            expect($subject->varName)->toBe('value');
+        });
+
+        it('can be set via as()', function () use ($subject) {
+            $result = $subject->as('foo');
+            expect($result)->toBe($subject);
+            expect($subject->varName)->toBe('foo');
+        });
+
+        it('can be set via alias()', function () use ($subject) {
+            $result = $subject->alias('foo');
+            expect($result)->toBe($subject);
+            expect($subject->varName)->toBe('foo');
+        });
+    });
+
+    describe('__call', function () use ($subject) {
+        it('executes checks', function () use ($subject) {
+            $result = $subject->isInt();
+            expect($result)->toBe($subject);
+            expect($subject->results)->toContainKey(0);
+            expect($subject->results[0])->toBeAnInstanceOf('Valit\Result\AssertionResult');
+        });
+        it('throws exception if check does not exist', function () use ($subject) {
+            $closure = function () use ($subject) {
+                $subject->nameOfCheckThatDoesNotExist();
+            };
+            expect($closure)->toThrow(new \BadMethodCallException);
+        });
+    });
+});
