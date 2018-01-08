af362bda9c320c720f6c2b80aafcb332533f3d84
Kim Ravn Hansen <moccalotto@gmail.com>
2018-01-08 08:01:00 +0100

    Kahlan LogicCheckProvider::checkLogic

---
+++ tests/unit/Providers/LogicCheckProvider/checkLogic.spec.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Kahlan\Spec\Suite;
+
+use Valit\Manager;
+use Valit\Logic\AnyOf;
+use Valit\Result\AssertionResult;
+use Valit\Providers\LogicCheckProvider;
+
+describe('LogicCheckProvider', function () {
+    $subject = new LogicCheckProvider();
+
+    describe('passesLogic', function () use ($subject) {
+        it('provides the correct assertions', function () use ($subject) {
+            expect($subject->provides())->toContainKey('isSuccessfulLogic');
+            expect($subject->provides())->toContainKey('passesLogic');
+            expect($subject->provides())->toContainKey('logic');
+        });
+
+        it('returns correct type', function () use ($subject) {
+            $result = $subject->checkLogic(
+                'foo',
+                new AnyOf(Manager::instance(), [true])
+            );
+            expect($result)->toBeAnInstanceOf(AssertionResult::class);
+        });
+
+        it('executes the logic', function () use ($subject) {
+            $result = $subject->checkLogic(
+                'foo',
+                new AnyOf(Manager::instance(), [true])
+            );
+            expect($result->success())->toBe(true);
+
+            $result = $subject->checkLogic(
+                'foo',
+                new AnyOf(Manager::instance(), [])
+            );
+            expect($result->success())->toBe(false);
+
+            $result = $subject->checkLogic(
+                'foo',
+                new AnyOf(Manager::instance(), [false])
+            );
+            expect($result->success())->toBe(false);
+
+            $result = $subject->checkLogic(
+                'foo',
+                new AnyOf(Manager::instance(), [false, true])
+            );
+            expect($result->success())->toBe(true);
+        });
+    });
+});
