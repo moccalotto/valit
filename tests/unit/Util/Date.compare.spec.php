e07d9e3d209e9c788aeb54643f3d4a8889598a38
Kim Ravn Hansen <krh@cego.dk>
2018-01-20 16:05:21 +0100

    test coverage of Date util

--- tests/unit/Util/Date.compare.spec.php
+++ tests/unit/Util/Date.compare.spec.php
@@ -31,4 +31,102 @@ describe('Valit\Util\Date', function () {
             expect($result)->toBe(0.0);
         });
     });
+
+    describe('::comparison()', function () {
+
+        it('less than', function () {
+
+            expect(
+                Date::comparison('<', '2000-01-01 00:00:00', '2000-01-01 00:00:01')
+            )->toBe(true);
+
+            expect(
+                Date::comparison('<', '2000-01-01 00:00:00', '2000-01-01 00:00:00')
+            )->toBe(false);
+
+            expect(
+                Date::comparison('<', '2000-01-01 00:00:01', '2000-01-01 00:00:00')
+            )->toBe(false);
+        });
+
+        it('less than or equal', function () {
+
+            expect(
+                Date::comparison('<=', '2000-01-01 00:00:00', '2000-01-01 00:00:01')
+            )->toBe(true);
+
+            expect(
+                Date::comparison('<=', '2000-01-01 00:00:00', '2000-01-01 00:00:00')
+            )->toBe(true);
+
+            expect(
+                Date::comparison('<=', '2000-01-01 00:00:01', '2000-01-01 00:00:00')
+            )->toBe(false);
+        });
+
+        it('greater than', function () {
+
+            expect(
+                Date::comparison('>', '2000-01-01 00:00:01', '2000-01-01 00:00:00')
+            )->toBe(true);
+
+            expect(
+                Date::comparison('>', '2000-01-01 00:00:00', '2000-01-01 00:00:00')
+            )->toBe(false);
+
+            expect(
+                Date::comparison('>', '2000-01-01 00:00:00', '2000-01-01 00:00:01')
+            )->toBe(false);
+        });
+
+        it('greater than or equal', function () {
+
+            expect(
+                Date::comparison('>=', '2000-01-01 00:00:01', '2000-01-01 00:00:00')
+            )->toBe(true);
+
+            expect(
+                Date::comparison('>=', '2000-01-01 00:00:00', '2000-01-01 00:00:00')
+            )->toBe(true);
+
+            expect(
+                Date::comparison('>=', '2000-01-01 00:00:00', '2000-01-01 00:00:01')
+            )->toBe(false);
+        });
+
+        it('equal', function () {
+
+            expect(
+                Date::comparison('=', '2000-01-01 00:00:00', '2000-01-01 00:00:00')
+            )->toBe(true);
+
+            expect(
+                Date::comparison('=', '2000-01-01 00:00:01', '2000-01-01 00:00:00')
+            )->toBe(false);
+
+            expect(
+                Date::comparison('=', '2000-01-01 00:00:00', '2000-01-01 00:00:01')
+            )->toBe(false);
+        });
+
+        it('throws if given an invalid operator', function () {
+
+            expect(function () {
+                Date::comparison('===', '00:00:00', '00:00:00');
+            })->toThrow(new \InvalidArgumentException());
+        });
+
+        it('does not throw if given a valid operator and valid dates', function () {
+
+            expect(function () {
+                Date::comparison('=', '00:00:00', '00:00:00');
+                Date::comparison('>', '00:00:00', '00:00:00');
+                Date::comparison('<', '00:00:00', '00:00:00');
+                Date::comparison('>=', '00:00:00', '00:00:00');
+                Date::comparison('<=', '00:00:00', '00:00:00');
+                Date::comparison('≥', '00:00:00', '00:00:00');
+                Date::comparison('≤', '00:00:00', '00:00:00');
+            })->not->toThrow();
+        });
+    });
 });
